5b8f2ff58d1dbcdec7e56afb982d580942f506fa
haythem05 <haythem.loueti@esprit.tn>
2024-02-27 19:38:27 +0100

    user spring

---
+++ src/main/java/com/example/pokerr/Controllers/AuthenticationController.java
@@ -0,0 +1,34 @@
+package com.example.pokerr.Controllers;
+
+import com.example.pokerr.Entities.AuthenticationRequest;
+import com.example.pokerr.Entities.AuthenticationResponse;
+import com.example.pokerr.Entities.RegisterRequest;
+import com.example.pokerr.Services.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+@CrossOrigin("http://localhost:4200")
+public class AuthenticationController {
+
+    private  final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register (
+            @RequestBody RegisterRequest request)
+    {
+        return ResponseEntity.ok(service.register(request));
+    }
+
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate (
+            @RequestBody AuthenticationRequest request)
+    {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
+}
