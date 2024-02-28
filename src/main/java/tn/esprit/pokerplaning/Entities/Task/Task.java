22e9b2901cd09b60dfd513cbc9c0ad634298f4db
haythem05 <haythem.loueti@esprit.tn>
2024-02-28 03:01:24 +0100

    fixed conflicts

--- src/main/java/tn/esprit/pokerplaning/Entities/Task/Task.java
+++ src/main/java/tn/esprit/pokerplaning/Entities/Task/Task.java
@@ -28,24 +28,21 @@ public class Task {
 
     private LocalDate endDate;
 
-<<<<<<< HEAD
+
     @Enumerated(EnumType.STRING)
     private Status status;
-=======
-    private int status;
->>>>>>> 374fea84c1f645878643d3e628e8c17aecb8511b
+
 
     @ManyToOne
     private Room roomTask;
 
     @ManyToOne
     private Sprint sprintTask;
-<<<<<<< HEAD
 
 
 
 
 
-=======
->>>>>>> 374fea84c1f645878643d3e628e8c17aecb8511b
+
+
 }
