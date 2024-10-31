eb59e8aac14aacbb6ad011563feec51a31482f5d
guerfala <mohamedtaher.guerfala@esprit.tn>
2024-10-31 04:16:12 +0100

    docker

--- src/main/java/tn/esprit/pokerplaning/Services/Room/PokerPlanningServices.java
+++ src/main/java/tn/esprit/pokerplaning/Services/Room/PokerPlanningServices.java
@@ -2,6 +2,9 @@ package tn.esprit.pokerplaning.Services.Room;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
